5588c357bff6b37a54ecbf65fad917fff73ef56d
Marcos Rigel <marcosrigel99@gmail.com>
2022-12-08 00:57:42 -0300

    projeto iniciado, calculadora ja definida

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Input from './components/Input';
+import Button from './components/Button';
+
+import { Container, Content, Row } from './styles';
+import { useState } from 'react';
+
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+
+  const handleAddNumber = (number) => {
+    setCurrentNumber{prev => '${number}${prev}'}
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        
+        <Row>
+          <Button label="x" onClick={() => handleAddNumber('')}/>
+          <Button label="/" onClick={() => handleAddNumber('')}/>
+          <Button label="C" onClick={() => handleAddNumber('')}/>
+          <Button label="L" onClick={() => handleAddNumber('')}/>
+        </Row>
+
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('')}/>
+          <Button label="8" onClick={() => handleAddNumber('')}/>
+          <Button label="9" onClick={() => handleAddNumber('')}/>
+          <Button label="-" onClick={() => handleAddNumber('')}/>
+        </Row>
+
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')}/>
+          <Button label="5" onClick={() => handleAddNumber('5')}/>
+          <Button label="6" onClick={() => handleAddNumber('6')}/>
+          <Button label="+" onClick={() => handleAddNumber('')}/>
+        </Row>
+
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')}/>
+          <Button label="2" onClick={() => handleAddNumber('2')}/>
+          <Button label="3" onClick={() => handleAddNumber('3')}/>
+          <Button label="=" onClick={() => handleAddNumber('=')}/>
+        </Row>
+
+      </Content>
+    </Container>
   );
 }
 
